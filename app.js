9b704a5f88637b31a4c4cb5caf3c14f613d1c0e1
Claudio Fuentes <imclaudfuen@gmail.com>
2018-11-19 21:30:42 -0500

    bitcoin API

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ fn();
 
 
 let btn = document.getElementById("btn");
+let btn2 = document.getElementById("btn2");
 let img = document.getElementById("photo");
+let price = document.getElementById("price");
 
 btn.addEventListener('click', function() {
     var XHR = new XMLHttpRequest();
@@ -38,5 +40,18 @@ btn.addEventListener('click', function() {
     }
 
     XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+    XHR.send();
+});
+
+btn2.addEventListener('click', function() {
+    var XHR = new XMLHttpRequest();
+
+    XHR.onreadystatechange = function(){
+        if(XHR.readyState == 4 && XHR.status == 200){
+            price.innerText = "$" + JSON.parse(XHR.responseText).bpi.USD.rate;
+        }
+    }
+
+    XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
     XHR.send();
 });
