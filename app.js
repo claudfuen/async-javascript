88e64405d123c8796acd8d85c2ba1d4df0e5d06b
Claudio Fuentes <imclaudfuen@gmail.com>
2018-11-19 20:53:57 -0500

    async start

---
+++ app.js
@@ -0,0 +1,28 @@
+/**
+ * Callback is a function we pass into an other fucntion
+ 
+Higher order functions take in callbacks as a param (fn)
+then executes the callback;
+fn();
+
+
+- Advanced Array methods
+- Browser Events
+- AJAX Requests
+- React Development
+
+ */
+
+
+//  function sendMessage (message, callback) {
+//      return callback(message);
+//  }
+
+//  sendMessage("Message for console", console.log);
+//  sendMessage("Message for alert", alert);
+
+//  var answer = sendMessage("Are you sure?", confirm); 
+
+
+
+var 
